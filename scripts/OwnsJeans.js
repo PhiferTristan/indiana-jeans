959d464a56c9ea0dca2bc5acbf6dc2228c3bab61
PhiferTristan <phifer.tristan@outlook.com>
2023-08-14 11:30:32 -0500

    added our first component function

---
+++ scripts/OwnsJeans.js
@@ -0,0 +1,6 @@
+export const ownsJeans = () => {
+  let html = "<h2>Do you own a pair of blue jeans?</h2>";
+  html += "<input type='radio' name='OwnsJeans' value='true' />Yes";
+  html += "<input type='radio' name='OwnsJeans' value='false' />No";
+  return html;
+};
