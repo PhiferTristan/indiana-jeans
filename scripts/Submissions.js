dbf9f5d9bbe11bb0d5046813f2e569b323068ab1
PhiferTristan <phifer.tristan@outlook.com>
2023-08-16 09:59:45 -0500

    added submissions list html

---
+++ scripts/Submissions.js
@@ -0,0 +1,16 @@
+export const SubmissionList = async () => {
+  // Get the submissions from your API
+  const response = await fetch("http://localhost:8088/submissions");
+  const submissions = await response.json();
+
+  // Iterate the submissions and create some <section> representations
+  let submissionHTML = "";
+  for (const submission of submissions) {
+    submissionHTML += `<section class="submission">
+                        <div>Owns Jeans? ${submission.ownsBlueJeans}</div>
+                        <div></div>Area type foreign key? ${submission.socioLocationId}
+                        </section>`;
+  }
+  return submissionHTML;
+  // Return the HTML string
+};
--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,6 @@
 import { OwnJeansChoices } from "./OwnsJeans.js";
 import { SaveSubmission } from "./SaveSubmissions.js";
+import { SubmissionList } from "./Submissions.js";
 import { LocationTypeChoices } from "./UrbanDweller.js";
 
 const container = document.querySelector("#container");
@@ -8,12 +9,13 @@ const render = async () => {
   const jeanOwnershipHTML = OwnJeansChoices();
   const locationsHTML = await LocationTypeChoices();
   const saveButtonHTML = await SaveSubmission();
+  const submissionListHTML = await SubmissionList();
 
   container.innerHTML = `
         ${jeanOwnershipHTML}
         ${locationsHTML}
         ${saveButtonHTML}
-
+        ${submissionListHTML}
     `;
 };
 
